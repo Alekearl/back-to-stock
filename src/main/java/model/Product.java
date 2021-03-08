94bb4d555d3e12377b6fe25d4b604343c6d3087e
Alekearl <alexandrhurko@gmail.com>
2021-03-08 22:03:04 +0200

    added Product

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.Objects;
+
+public class Product {
+
+    private final String id;
+    private final ProductCategory category;
+
+    public Product(String id, ProductCategory category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && category == product.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, category);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", category=" + category +
+                '}';
+    }
+}
